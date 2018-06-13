32ea3fc3124c74c2a51c9ed156d7d049e703c312
Sebastien Fortas <sebastien@fortas.org>
2018-06-13 17:57:21 -0400

    zsh completions

--- canopus/.zsh/50-completion.zsh
+++ canopus/.zsh/50-completion.zsh
@@ -1,22 +1,41 @@
-zmodload -i zsh/complist
-
+# autocomplete
+setopt complete_in_word
+setopt always_to_end
+setopt path_dirs
+setopt auto_menu
+setopt auto_list
 unsetopt menu_complete
 unsetopt flowcontrol
 
-setopt auto_menu
-setopt complete_in_word
-setopt always_to_end
+# use caching
+zstyle ':completion:*' use-cache true
+zstyle ':completion:*' cache-path ~/.cache/zsh
+
+zstyle ':completion:*' special-dirs true
 
 zstyle ':completion:*' menu select
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-dirs-first true
+zstyle ':completion:*:options' description true
+zstyle ':completion:*:options' auto-description 'specify: %d'
+zstyle ':completion:*:corrections' format '%F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:warnings' format '%F{red}-- no matches found --%f'
+zstyle ':completion:*' verbose true
+
 # case insensitive search
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z]#)*=01;34;=0=01'
-
-# process selection list
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:*:*:processes' command 'ps -u $USER -o pid,user,args -w'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:kill:*' force-list always
 
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
-zstyle ':completion::complete:*' use-cache true
+# pick image files first, if fail, pick all files.
+#zstyle ':completion:*:*:pinta:*' file-patterns '*.(jpg|JPG|gif|GIF|png|PNG):image-files' '%p:all-files'
+
+
+autoload -Uz compinit
+compinit
